a137db053a2f234771a76a79aae1debac833350f
khamb <khadim1996@icloud.com>
2017-11-12 20:14:30 -0500

    Initial Commit

---
+++ csi2772_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  csi2772_project
+//
+//  Created by Khadim Mbaye on 11/12/17.
+//  Copyright © 2017 Khadim Mbaye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
